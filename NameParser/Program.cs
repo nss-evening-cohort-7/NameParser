d318b2e5ed97dcc7f9fe77501527ab0109eaae48
nathan gonzalez <ncgonz@gmail.com>
2018-08-27 18:20:49 -0500

    saturday's code

--- NameParser/Program.cs
+++ NameParser/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using NameParser.Stuff;
+using System;
 
 namespace NameParser
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var x = new Thing();
+            var y = new Thing();
+
+            x.MyProperty = 4;
+            Console.WriteLine(x.MyProperty);
+            
+            y.AssignTheNumber(1110);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ NameParser/Stuff/Thing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameParser.Stuff
+{
+    class Thing
+    {
+        int _myNumber = 1;
+
+        public int MyProperty { get; set; }
+
+        public void AssignTheNumber(int myNumber)
+        {
+            _myNumber = myNumber;
+        }
+        
+        string GetStuff()
+        {
+            var x = _myNumber;
+
+            return "stuff";
+        }
+    }
+}
